d3d0fa779288e079d862a202eb1981323692713c
Fabian Da Silva <fabian.da.silva@hotmail.com>
2022-08-26 20:07:47 -0300

    Primer desafio

---
+++ main.js
@@ -0,0 +1,37 @@
+/* Clase 1 */
+//let numberOne = prompt('Ingrese el primer numero');
+//let numberTwo = prompt('Ingrese el segundo numero');
+//let suma = parseInt(numberOne) + parseInt(numberTwo);
+//alert(suma);
+
+/* Clase 2 */
+//let nombre = prompt("ingrese su nombre").toLowerCase();
+//
+//if(nombre === "andrés"){
+//  alert("Hola Felipe");
+//}else{
+//  alert("quien eres?");
+//}
+//
+//alert("terminamos");
+/* NaN = no es un numero */
+//parseInt = numero entero
+//parseFloat = numero con coma
+
+/* ejercicio en clase 1 */
+//let number = prompt('Ingrese un número mayor a 1000');
+//
+//if (number > 1000) {
+//	alert('Ingresaste correctamente');
+//} else {
+//	alert('Por favor ingresa un número dentro del rango');
+//}
+
+/* ejercicio en clase 2 */
+//let text = prompt('Adivina la palabra correcta');
+//
+//if (text === 'Hola' || text === 'HOLA') {
+//	alert('Felicitaciones! ingresaste la palabra correcta');
+//} else {
+//	alert('Segui intentando');
+//}
