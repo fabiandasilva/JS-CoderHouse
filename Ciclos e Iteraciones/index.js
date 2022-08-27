e14ab8ffe5d9fed05b289c65a489bba674191f51
Fabian Da Silva <fabian.da.silva@hotmail.com>
2022-08-26 21:40:29 -0300

    3. Ciclos/iteraciones

---
+++ Ciclos e Iteraciones/index.js
@@ -0,0 +1,23 @@
+/**********************************
+ *Ejercicio de concatenar palabras*
+ *********************************/
+//let intput = prompt('Ingrese una plabra');
+//let output = '';
+//while (intput != 'ESC') {
+//	alert('Ingresaste:\n' + intput + '\n');
+//	output = output + ' ' + intput;
+//	intput = prompt('Ingrese otra palabra, Escriba ESC para finalizar el ciclo');
+//}
+//alert(output);
+
+
+/**********************
+ *Ejercicio del Saludo*
+ *********************/
+//let number = prompt('Ingrese un numero');
+//greeting = '';
+//
+//for (let index = 0; index < number; index++) {
+//	greeting += 'Hola \n ';
+//}
+//alert(greeting);
